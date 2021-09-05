32d9ce17ec33e88435f19000f41214cd8891343d
Vladimir Bessonov <bessmy@gmail.com>
2021-09-05 12:35:07 +0300

    feat: st-mac-address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  const arrOfDigits = n.split('-');
+  if (arrOfDigits.length !== 6) return false;
+  let result = true;
+  arrOfDigits.forEach((item) => {
+    if (Number.isNaN(parseInt(item, 16))) result = false;
+  });
+  return result;
 }
