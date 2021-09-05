531c0e4b7e221d9b6d1bd84aa2144dea8b8172ad
Vladimir Bessonov <bessmy@gmail.com>
2021-09-05 11:23:08 +0300

    feat: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,42 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!(Array.isArray(arr))) throw new Error(`'arr' parameter must be an instance of the Array!`);
+  let flag = false;
+  let flag2 = false;
+  const result = arr.reduce((acc, item, i) => {
+    switch (item) {
+      case '--discard-next':
+        if (arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev') flag2 = true;
+        flag = true;
+        return acc;
+      case '--discard-prev':
+        if (flag2 === true) {
+          flag2 = false;
+          return acc;
+        }
+        if (i > 0) acc.pop();
+        return acc;
+      case '--double-next':
+        if (i < arr.length - 1) acc.push(arr[i + 1]);
+        return acc;
+      case '--double-prev':
+        if (flag2 === true) {
+          flag2 = false;
+          return acc;
+        }
+        if (i > 0) acc.push(arr[i - 1]);
+        return acc;
+      default:
+        if (flag === true) {
+          flag = false;
+          return acc;
+        } else {
+          acc.push(item);
+          return acc;
+        }
+    }
+  }, []);
+  return result;
 }
